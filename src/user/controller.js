c361687c21c97b5fc6ddf3e2e27821f7715e5a44
Kenneth Siyabonga Ncube <160495796+ghost2468developer@users.noreply.github.com>
2026-04-16 21:37:43 +0200

    reset soon
    Change project structure

---
+++ src/user/controller.js
@@ -0,0 +1,17 @@
+const register = async (req, res) => {
+    res.send("Register User")
+}
+
+const login = async (req, res) => {
+    res.send("Login User")
+}
+
+const logout = (req, res) => {
+    res.send("Logout User")
+}
+
+const updateUser = (req, res) => {
+    res.send("Update User")
+}
+
+const 
